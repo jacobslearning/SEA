111f8965100070417d280e30a32f0556cffe0fb9
Jacob S <jacobslearning12@gmail.com>
2025-05-24 21:13:02 +0100

    get db func

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ import sqlite3 as sql
 
 app = Flask(__name__)
 app.secret_key = 'test'
+app.config['DATABASE'] = 'database.db'
+
+# database helper functions
+
+def get_db():
+    if 'db' not in g:
+        g.db = sql.connect(app.config['DATABASE'])
+        g.db.row_factory = sql.Row  # So you can access columns by name
+    return g.db
 
 if __name__ == '__main__':
    app.run(debug = True)
