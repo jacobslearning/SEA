dfb7614bde7648a8e50dcfa1576999967c4a6e7b
Jacob S <jacobslearning12@gmail.com>
2025-05-24 21:14:49 +0100

    database helper functions

--- app.py
+++ app.py
@@ -10,8 +10,26 @@ app.config['DATABASE'] = 'database.db'
 def get_db():
     if 'db' not in g:
         g.db = sql.connect(app.config['DATABASE'])
-        g.db.row_factory = sql.Row  # So you can access columns by name
+        g.db.row_factory = sql.Row
     return g.db
+
+@app.teardown_appcontext
+def close_db(error=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+def execute_db(query, args=()):
+    db = get_db()
+    cur = db.execute(query, args)
+    db.commit()
+    return cur.lastrowid
 
 if __name__ == '__main__':
    app.run(debug = True)
